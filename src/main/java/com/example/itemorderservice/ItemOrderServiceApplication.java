05e0db94d513102764fda600f20b6b03a7509b6f
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-11-23 23:08:40 +0900

    Initial commit

---
+++ src/main/java/com/example/itemorderservice/ItemOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.itemorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/itemorderservice/ItemOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itemorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
