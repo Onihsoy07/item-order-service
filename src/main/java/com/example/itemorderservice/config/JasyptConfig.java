1a7d9d4138fe8a260a5ffcc7e2eb07c2a2919747
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-11-24 23:25:16 +0900

    수정 : 프로퍼티 암호화 암호키 분리

--- src/main/java/com/example/itemorderservice/config/JasyptConfig.java
+++ src/main/java/com/example/itemorderservice/config/JasyptConfig.java
@@ -6,16 +6,24 @@ import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableEncryptableProperties
 public class JasyptConfig {
 
+    private String jasyptKey = getJasyptEncryptorPassword();
+
     @Bean("jasyptStringEncryptor")
     public StringEncryptor stringEncryptor() {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword("password");
+        config.setPassword(jasyptKey);
         config.setAlgorithm("PBEWithMD5ANdDES");
         config.setKeyObtentionIterations("1000");
         config.setPoolSize("1");
@@ -25,6 +33,17 @@ public class JasyptConfig {
         config.setStringOutputType("base64");
         encryptor.setConfig(config);
         return encryptor;
+    }
+
+    //Resource에서 jasypt password 받기
+    private String getJasyptEncryptorPassword() {
+        try {
+            ClassPathResource resource = new ClassPathResource("jasypt-encryptor-password.txt");
+            return Files.readAllLines(Paths.get(resource.getURI())).stream()
+                    .collect(Collectors.joining(""));
+        } catch (IOException e) {
+            throw new RuntimeException("Not found Jasypt password file.");
+        }
     }
 
 }
